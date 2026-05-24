15b59755cbf786d572a378c6bfd87554d5f9aa19
11abhinav <48574111+11abhinav@users.noreply.github.com>
2026-05-24 19:11:04 +0530

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID"
+
+WATCHLIST_PATH = "data/elite_fundamental_watchlist.parquet"
+DB_PATH = "data/alerts.db"
+
+BREAKOUT_SCORE_THRESHOLD = 70
