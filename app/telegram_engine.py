fb7c60f5fc75fbda14333456343149b8aeacf6a0
11abhinav <48574111+11abhinav@users.noreply.github.com>
2026-05-24 20:01:41 +0530

    Update telegram_engine.py

--- app/telegram_engine.py
+++ app/telegram_engine.py
@@ -1,15 +1,41 @@
+# =====================================================================================
+# app/telegram_engine.py
+# =====================================================================================
+
 import requests
+
 from config import BOT_TOKEN, CHAT_ID
 
+# =====================================================================================
+# SEND TELEGRAM MESSAGE
+# =====================================================================================
 
 def send_telegram_message(message):
 
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    try:
 
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": message,
-        "parse_mode": "Markdown"
-    }
+        url = (
 
-    requests.post(url, json=payload)
+            f"https://api.telegram.org/"
+            f"bot{BOT_TOKEN}/sendMessage"
+        )
+
+        payload = {
+
+            "chat_id": CHAT_ID,
+
+            "text": message
+        }
+
+        requests.post(
+
+            url,
+
+            json=payload,
+
+            timeout=10
+        )
+
+    except Exception as e:
+
+        print(f"❌ TELEGRAM ERROR -> {e}")
