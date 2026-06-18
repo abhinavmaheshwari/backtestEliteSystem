1027f1f618d28e13ca812ce37f76e9de8a74aa6a
abhinavmaheshwari <abhinav2788@gmail.com>
2026-06-18 20:56:26 +0530

    feat: allow POST requests in CORS header configuration

--- app/dashboard_server.py
+++ app/dashboard_server.py
@@ -154,7 +154,7 @@ def api_messages_read():
 def add_headers(response):
     response.headers["Access-Control-Allow-Origin"]  = "*"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type"
-    response.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     response.headers["Cache-Control"]                = "no-cache, no-store, must-revalidate"
     response.headers["Pragma"]                       = "no-cache"
     return response
